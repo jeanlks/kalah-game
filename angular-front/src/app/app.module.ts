2eb0d4e387c75ce93ea2c5e3362c3bcb13a00dc6
Jean Lucas Monte Carvalho <jmontecarvalho@gmail.com>
2019-12-17 11:11:39 -0200

    Mudança home component

--- angular-front/src/app/app.module.ts
+++ angular-front/src/app/app.module.ts
@@ -4,7 +4,9 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { MatCardModule } from '@angular/material/card';
 import {MatGridListModule} from '@angular/material/grid-list';
-import { MatInputModule, MatButtonModule, MatSelectModule, MatIconModule, MatDialog, MatDialogModule } from '@angular/material';
+import { MatInputModule, MatButtonModule, MatSelectModule, MatIconModule,
+         MatDialog, MatDialogModule, MatProgressSpinnerModule, MatMenuModule, MatTableModule, MatToolbarModule 
+} from '@angular/material';
 import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -12,6 +14,7 @@ import { NoopAnimationsModule } from '@angular/platform-browser/animations';
 import { HttpClientModule } from '@angular/common/http';
 import { HomeComponent } from './home/home.component';
 import { BoardComponent } from './board/board.component';
+import { CommonModule } from '@angular/common';
 
 @NgModule({
   declarations: [
@@ -32,7 +35,20 @@ import { BoardComponent } from './board/board.component';
     MatCardModule,
     MatGridListModule,
     HttpClientModule,
-    MatDialogModule
+    MatDialogModule,
+    MatProgressSpinnerModule
+  ],
+  exports: [
+     CommonModule,
+     MatToolbarModule,
+     MatButtonModule,
+     MatCardModule,
+     MatInputModule,
+     MatDialogModule,
+     MatTableModule,
+     MatMenuModule,
+     MatIconModule,
+     MatProgressSpinnerModule
   ],
   entryComponents: [AppModalComponent],
   providers: [AppService],
